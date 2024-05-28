72dab24eda55fb15302dd1573ef5f4445157624a
terr0byte <il.fa201114@yandex.ru>
2024-05-28 12:45:30 +0300

    refactor: update functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+//DOM узлы
 const cardContainer = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
-function createCard(cardImage, cardTitle, deleteFunc) {
+//Функция создания карточки
+function createCard(item, { deleteFunc }) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardImage = cardElement.querySelector('.card__image');
 
-    cardElement.querySelector('.card__image').src = cardImage;
-    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
 
     deleteButton.addEventListener('click', () => {
         deleteFunc(cardElement);
     })
 
-    cardContainer.append(cardElement);
+    return cardElement;
+    ;
 }
 
-// @todo: Функция удаления карточки
+//Функция удаления карточки
 function deleteCard(card) {
     card.closest('.card').remove();
 }
 
-// @todo: Вывести карточки на страницу
-initialCards.forEach((item, i) => {
-    createCard(item.link, item.name, deleteCard);
+//Вывести карточки на страницу
+initialCards.forEach((item) => {
+    cardContainer.append(createCard(item, { deleteFunc: deleteCard }));
 })
